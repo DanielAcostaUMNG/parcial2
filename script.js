436d685f731dc6016e9dd844e811f6a83af6a7c2
DANIEL ACOSTA <est.daniel.aacosta@unimilitar.edu.co>
2026-04-24 15:05:07 +0000

    Se crea y ejecuta funcion Bresenham en el codigo

--- script.js
+++ script.js
@@ -18,9 +18,32 @@ function drawPixel(ctx, x, y, color = "#000000") {
  * @param {number} x1, y1 - Coordenadas finales
  * @returns {void}
  */
-function bresenhamLine(x0, y0, x1, y1, color) {
-    // Desarrollo del estudiante
-}
+function bresenhamLine(x0, y0, x1, y1, color = "#000000")      
+    {
+        let dx = Math.abs(x1 - x0);
+        let dy = Math.abs(y1 - y0);
+        //Esto es para hacer la direccion de avance en cada eje 
+        let sx = (x0 < x1) ? 1 : -1; 
+        let sy = (y0 < y1) ? 1 : -1;
+        //Con esto iniciamos el error, que es la diferencia entre dx y dy, esto nos ayudara a decidir cuando avanzar en y mientras avanzamos en x
+        let err = dx - dy;
+
+        while (true) {
+            drawPixel(ctx, x0, y0, color); // Dibuja el píxel actual
+
+            if (x0 === x1 && y0 === y1) break; // Si hemos llegado al punto final, terminamos
+
+            let err2 = 2 * err; // Duplicamos el error para comparar
+            if (err2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (err2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
 
 /**
  * Calcula los vértices de un polígono regular.
